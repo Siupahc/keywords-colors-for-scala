76e1c4c688a2e736db071b20266349a937c1b8cb
GitHub Action <action@github.com>
2026-01-02 13:26:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/siupahc/keywordscolorsforscala/services/MyProjectService.kt
+++ src/main/kotlin/com/github/siupahc/keywordscolorsforscala/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.siupahc.keywordscolorsforscala.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.siupahc.keywordscolorsforscala.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
